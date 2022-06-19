31e224e0fe17015e2c1a1de0782b4e494e530919
Nathan099-dev <82057687+Nathan099-dev@users.noreply.github.com>
2022-06-19 15:12:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
 const botao = document.querySelector('.icon-menu-mobile');
 
-botao.addEventListener('click', () =>{
-    const item = document.querySelector('.menu-mobile');
-    if (item.classList.contains('show')) {
-        item.classList.add('hide');
-        item.classList.remove('show');
-
-    }else{
-        item.classList.add('show');
-        item.classList.remove('hide');
-    }
-   
+botao.addEventListenner('click', function abrirmenu(a){
+   document.classList.add('show');
 });
+
+return abrirMenu();
